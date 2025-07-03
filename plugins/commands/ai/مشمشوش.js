9b0c2c8078611a1250bec9505837af102754c172
rakomoro <yisfmohamm4466@gmail.com>
2025-07-03 20:31:40 +0200

    Update مشمشوش.js

--- plugins/commands/ai/مشمشوش.js
+++ plugins/commands/ai/مشمشوش.js
@@ -15,7 +15,7 @@ async function onCall({ message, args, getLang }) {
   const text = args.join(' ');
   
   // الردود المحلية على الأسئلة الشائعة
-  if (!text || text === '') return message.reply("مرحبًا! كيف يمكنني مساعدتك؟ 🐸");
+  if (!text || text === '') return message.reply(" مرحبا بوت مشمش خاص بأنمي سانين شنو بدك 🐢؟  https://facebook.com/groups/1262520641964913/");
   if (text.includes('كيفك') || text.includes('كيف حالك')) return message.reply("تمام، أنا بخير. شكرًا على السؤال! 🐸");
   if (text.includes('من أنت')) return message.reply("أنا مشمش، مصمم لمساعدتك في الحصول على المعلومات. 🐸");
 
