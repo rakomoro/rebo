595624dad88fcd924b98292aae753355f9b5a9c7
rakomoro <yisfmohamm4466@gmail.com>
2025-07-12 15:06:04 +0200

    Update تزوجني.js

--- plugins/commands/general/تزوجني.js
+++ plugins/commands/general/تزوجني.js
@@ -1,3 +1,3 @@
 export default function ({ message }) {
-    global.random(0, 5) === 3 ? message.reply("نحن مجرد اصدقاء يا صديقتي 🌚💔") : message.reply("We are just friends 😔");
+    global.random(0, 5) === 3 ? message.reply("نحن مجرد اصدقاء يا صديقتي 🌚💔") : message.reply("نحن مجرد اصدقاء يا صديقتي 🌚💔🐢");
 }
