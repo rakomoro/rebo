d8b97913661ac63c616878f9729135fc1a79cd8b
rakomoro <yisfmohamm4466@gmail.com>
2025-08-04 03:13:45 +0200

    Create anime.js

---
+++ plugins/anime.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const config = {
+  name: 'انمي', 
+  permissions: [2],
+};
+
+async function onCall({ message }) {
+  try {
+    const response = await axios.get("https://rapido.zetsu.xyz/api/anime-photo", {
+      responseType: 'stream'
+    });
+    
+    await message.reply({
+      attachment: response.data
+    });
+    
+  } catch (error) {
+    await message.reply(error);
+  }
+}
+
+export default {
+  config,
+  onCall
+};
